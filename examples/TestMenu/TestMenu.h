5fefe64779e2b6962ce11ac21360199db94f19a0
David Sanz Kirbis <info@dasaki.com>
2016-12-05 11:11:07 +0100

    Adapted code for Adafruit_SSD1306 library and 0.96" OLED display

--- examples/TestMenu/TestMenu.h
+++ examples/TestMenu/TestMenu.h
@@ -1,44 +1,44 @@
-// required for "prog_char" and "PROGMEM"
+// required for "const char" and "PROGMEM"
 #include <avr/pgmspace.h>
 
 // texts for menus
 
-prog_char itmBack[] PROGMEM = "< Back";
-prog_char itmOn[] PROGMEM = "On";
-prog_char itmOff[] PROGMEM = "Off";
-prog_char itmEnabled[] PROGMEM = "Enabled";
-prog_char itmDisabled[] PROGMEM = "Disabled";
+const char itmBack[] PROGMEM = "< Back";
+const char itmOn[] PROGMEM = "On";
+const char itmOff[] PROGMEM = "Off";
+const char itmEnabled[] PROGMEM = "Enabled";
+const char itmDisabled[] PROGMEM = "Disabled";
 
-prog_char itmRoot[] PROGMEM = "Root menu";
-prog_char itmSubmenu1[] PROGMEM = "Submenu 1";
-prog_char itmSubmenu2[] PROGMEM = "Submenu 2";
-prog_char itmSubmenu3[] PROGMEM = "Submenu 3";
-prog_char itmMessageBox[] PROGMEM = "Message box";
-prog_char itmItem1[] PROGMEM = "Item 1";
-prog_char itmItem2[] PROGMEM = "Item 2";
-prog_char itmItem3[] PROGMEM = "Item 3";
-prog_char itmItem4[] PROGMEM = "Item 4";
-prog_char itmItem5[] PROGMEM = "Item 5";
+const char itmRoot[] PROGMEM = "Root menu";
+const char itmSubmenu1[] PROGMEM = "Submenu 1";
+const char itmSubmenu2[] PROGMEM = "Submenu 2";
+const char itmSubmenu3[] PROGMEM = "Submenu 3";
+const char itmMessageBox[] PROGMEM = "Message box";
+const char itmItem1[] PROGMEM = "Item 1";
+const char itmItem2[] PROGMEM = "Item 2";
+const char itmItem3[] PROGMEM = "Item 3";
+const char itmItem4[] PROGMEM = "Item 4";
+const char itmItem5[] PROGMEM = "Item 5";
 
 ////////////////////////////////////////////////////////////////
 // menus - first item is menu title and it does not count toward cnt
 
-PROGMEM const char * mnuRoot[] = {
+PROGMEM const char * const mnuRoot[] = {
   itmRoot,
   itmSubmenu1,itmSubmenu2,itmSubmenu3,itmMessageBox};
 PROGMEM const int cntRoot = 4;
 
-PROGMEM const char * mnuSubmenu1[] = {
+PROGMEM const char * const mnuSubmenu1[] = {
   itmSubmenu1,
   itmItem1,itmItem2,itmItem3,itmItem4,itmItem5,itmBack};
 PROGMEM const int cntSubmenu1 = 6;
 
-PROGMEM const char * mnuSubmenu2[] = {
+PROGMEM const char * const mnuSubmenu2[] = {
   itmSubmenu2,
   itmOn,itmOff,itmBack};
 PROGMEM const int cntSubmenu2 = 3;
 
-PROGMEM const char * mnuSubmenu3[] = {
+PROGMEM const char * const mnuSubmenu3[] = {
   itmSubmenu3,
   itmEnabled,itmDisabled,itmBack};
 PROGMEM const int cntSubmenu3 = 3;
